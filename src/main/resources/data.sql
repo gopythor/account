6e151f6d9917fec603880948373babd75274e778
SoliDeoHoneretGloria <iphone_1st@naver.com>
2023-03-12 22:40:25 +0100

    Lock 업데이트

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into account_user(id, name, created_At, updated_At)
+values (1, 'Pororo', now(), now());
+insert into account_user(id, name, created_At, updated_At)
+values (2, 'Lupi', now(), now());
+insert into account_user(id, name, created_At, updated_At)
+values (3, 'Eddie', now(), now());
